07c015f0fd75abb04ca910dcaf22a77fba2f1f23
Lucie Langlois <lucie.langlois@hotmail.fr>
2021-06-24 15:04:39 +0200

    route et page error homepage result

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,27 @@ router.get('/', function(req, res, next) {
   res.render('login');
 });
 
+/* homepage partie 2 avec le calendrier */
+router.get('/homepage', function (req,res,next){
+
+res.render('homepage');
+})
+
+/*formulaire de recherche qui va chercher dans la BDD  */
+router.post('/booking', function (req,res,next){
+
+
+res.redirect('/result');
+} )
+
+/*adressage des pages de résultats*/
+router.get('/result', function (req,res,next){
+  res.render('result');
+  })
+/*adressage des pages d'erreursg*/
+router.get('/error', function(res,req,next) {
+res.render('error');
+})
+
 
 module.exports = router;
